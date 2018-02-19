07b65584c1a95767efa621a91369749c4f36b429
Hilvitz, Spencer <SPENCER_HILVITZ@homedepot.com>
2018-02-19 15:02:15 -0500

    Add template literals

---
+++ TemplateLiterals.js
@@ -0,0 +1,14 @@
+// es5
+var a = 5;
+var b = 10;
+console.log('Fifteen is ' + (a + b) + ' and\nnot ' + (2 * a + b) + '.');
+// "Fifteen is 15 and
+// not 20."
+
+// es6
+var a = 5;
+var b = 10;
+console.log(`Fifteen is ${a + b} and
+not ${2 * a + b}.`);
+// "Fifteen is 15 and
+// not 20."
